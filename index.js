9a045e9a3622b9f6b7666f7e3b6f8cc09b63bb03
Divyanshu <sdivyanshu105@gmail.com>
2022-05-06 03:40:33 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+function app(){
+    var sum=0;
+    for(var i=0; i<arguments.length;i++)
+    {
+         sum=sum+arguments[i];
+    }
+    return sum
+   
+}
+
+var total= app(3,5,8);
+console.log(total);
